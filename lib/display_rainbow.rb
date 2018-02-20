2312538bb7b4bc2a67d9009823cc9b57ddab5b6e
Elizabeth Eckels <elizabeth.r.eckels@gmail.com>
2018-02-20 02:28:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 #Option 1
-  def display_rainbow([0,1,2,3,4,5,6])
-  end
+#  def display_rainbow([0,1,2,3,4,5,6])
+#  end
 
 #Option 2
-#colors = ["red","orange","yellow","green","blue","indigo","violet"]
-#  def display_rainbow(colors)
-#  end
+colors = ["red","orange","yellow","green","blue","indigo","violet"]
+  def display_rainbow(colors)
+  end
 
 #Option 3
 #def display_rainbow(["color0","color1","color2","color3","color4","color5","color6"])
